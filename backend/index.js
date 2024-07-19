3c62c48f122660d40948ff32491f5ee0e76b68d6
Yashi Gupta <yashig52@gmail.com>
2024-07-19 10:29:16 +0530

    old version

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin:"https://twitter-frontend-4inj.onrender.com",
+    origin:"http://localhost:3000",
     credentials:true
 }
 app.use(cors(corsOptions));
